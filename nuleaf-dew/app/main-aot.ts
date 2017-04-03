a2a75ea57a3f9e37189d2e7b6ac5217ddf90072c
Tuan Pham <ttpham0111@gmail.com>
2017-04-02 22:00:24 -0700

    Always server production when using main.aot

--- nuleaf-dew/app/main-aot.ts
+++ nuleaf-dew/app/main-aot.ts
@@ -4,6 +4,5 @@ import { enableProdMode } from '@angular/core';
 import { AppModuleNgFactory } from '../aot/app/app.module.ngfactory';
 
 
-// Enable production mode unless running locally
-if (!/localhost/.test(document.location.host)) enableProdMode();
+enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
