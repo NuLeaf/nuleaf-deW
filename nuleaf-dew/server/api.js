874c8feea3ddcb2cd1346cff95b7e3fb6e7b6b44
Tuan Pham <ttpham0111@gmail.com>
2017-04-09 20:51:47 -0700

    Add emailto field for contacts page.

--- nuleaf-dew/server/api.js
+++ nuleaf-dew/server/api.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+
 router.get('/', function(req, res) {
-  res.send({status: 'OK'});
+  res.status(200).json({status: 'OK'});
 });
 
 module.exports = router;
